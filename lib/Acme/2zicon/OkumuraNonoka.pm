5c83d333751cbadcc20d94ec29dda0e4bd21fe4e
KANEKO Tatsuya <catatsuy@catatsuy.org>
2014-10-11 06:34:15 +0000

    Import of CATATSUY/Acme-2zicon-0.2.1 from CPAN.
    gitpan-cpan-distribution: Acme-2zicon
    gitpan-cpan-version:      0.2.1
    gitpan-cpan-path:         CATATSUY/Acme-2zicon-0.2.1.tar.gz
    gitpan-cpan-author:       CATATSUY
    gitpan-cpan-maturity:     released

--- lib/Acme/2zicon/OkumuraNonoka.pm
+++ lib/Acme/2zicon/OkumuraNonoka.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use base qw(Acme::2zicon::Base);
 
-our $VERSION = '0.2';
+our $VERSION = '0.2.1';
 
 sub info {
     my $self = shift;
@@ -18,7 +18,7 @@ sub info {
         birthday       => $self->_datetime_from_date('2001.01.04'),
         blood_type     => 'O',
         hometown       => '東京都',
-        introduction   => "アイドルオタクの進化系。毎日がビッグバン。せーの！\n ＼どーん／\n[hometown]出身の最年少[age]歳。\nののたこと[name_ja]です。",
+        introduction   => "アイドルオタクの進化系。毎日がビッグバン。せーの！\n＼どーん／\n[hometown]出身の最年少[age]歳。\nののたこと[name_ja]です。",
         twitter        => 'okumura_nonoka',
     );
 }
